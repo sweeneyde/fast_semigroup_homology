1950cd3954e9dcd3bcd610fefcbcfb657f69cc0e
Dennis Sweeney <sweeney.427@osu.edu>
2026-08-09 16:26:29 -0400

    homology_with_generators

---
+++ fast_semigroup_homology/cup_products/homology_with_generators.py
@@ -0,0 +1,145 @@
+from mutable_lattice import Vector, Lattice, transpose, xgcd, relations_among
+
+def _hnf(vectors):
+    """Apply row operations to convert to HNF"""
+    if not vectors:
+        return vectors
+    return Lattice(len(vectors[0]), vectors, maxrank=len(vectors)).get_basis()
+
+def _augment_id(vectors):
+    """Given a matrix A of row vectors, get the HNF of the augmented matix [A | id]"""
+    R = len(vectors)
+    return [Vector(v.tolist() + [0]*i + [1] + [0]*(R-i-1))
+            for i, v in enumerate(vectors)]
+
+def _inverse(vectors):
+    """Get the inverse of an integer matrix."""
+    N = len(vectors)
+    augmented = _hnf(_augment_id(vectors))
+    for i, v in enumerate(augmented):
+        for j in range(N):
+            if v[j] and i != j:
+                raise ValueError("Not invertible")
+    return [Vector(aug.tolist()[N:]) for aug in augmented]
+
+def _smith_with_coefficients_unnormalized(N0, vectors):
+    """Find a diagonal form of a matrix, along with the matrix of row operations used.
+
+    Given a matrix A of row vectors, apply row operations to [A | id],
+    along with column operations to A only, to get a matrix [D | S],
+    where D=S*A*T is diagonal (but not necessarily square),
+    and T (not provided) is the matrix of column operations used.
+    Return the diagonal entries of D, along with the row vectors of S.
+    """
+    R = len(vectors)
+    # Try column ops first because they're easier
+    columns = transpose(N0, vectors)
+    columns = _hnf(columns)
+    N = len(columns)
+    del vectors
+    augmented = _hnf(_augment_id(transpose(R, columns)))
+    # Repeat column ops and row ups until diagonal. Because we did the initial column steps,
+    # the first N columns already have rank N, so we only need to check to the right
+    # of the main diagonal.
+    while any(any(map(aug.__getitem__, range(i + 1, N))) for i, aug in enumerate(augmented)):
+        aug_columns = transpose(N+R, augmented)
+        aug_columns[:N] = _hnf(aug_columns[:N])
+        augmented = transpose(R, aug_columns)
+        augmented = _hnf(augmented)
+    assert N <= R
+    invariants = [augmented[i][i] for i in range(N)] + [0] * (R - N)
+    coefficient_vectors = [Vector(v.tolist()[N:]) for v in augmented]
+    return invariants, coefficient_vectors
+
+def _normalize_pair(d1, v1, d2, v2):
+    """
+    If [[d1,0],[0,d2]] is in SNF, with associated coefficient vectors v1 and v2,
+    Fix the divisibility to produce the SNF [[gcd(d1,d2),0],[0,lcm(d1,d2)]],
+    with the new associated coefficient vectors.
+    """
+    assert d1 and d2
+    assert d2 % d1 != 0
+    x, y, g = xgcd(d1, d2)
+    # x and y satisfy:
+    #   x * d1 + y * d2 == g
+    #   (x//g)*d1 + (y//g)*d2 == 1
+    # Implicitly do a column op, then apply an invertible matrix of row ops:
+    # [    x      y] [d1  0|---v1---]  == [ g      d2*y |---------x*v1+y*v2----------]
+    # [-d2//g d1//g] [d2 d2|---v2---]     [ 0  d1*d2//g |---(-d2//g)*d1+(d1//g)*v2---]
+    # Since g divides d2 divides d2*y, an implicit column op restores diagonality.
+    return g, x*v1+y*v2, d1//g*d2, (-d2//g)*v1+(d1//g)*v2
+
+def _normalize_smith_with_coefficients(invariants, coefficient_vectors):
+    """
+    Repeatedly apply _normalize_pair to apply the SNF divisibility constraint
+    to diagonal matrix with associated coefficient vectors
+    """
+    vecs, invars, zeros = [], [], []
+    for d, v in zip(invariants, coefficient_vectors, strict=True):
+        if d:
+            assert d > 0
+            invars.append(d)
+            vecs.append(v)
+        else:
+            zeros.append(v)
+    while True:
+        perm = sorted(range(len(invars)), key=invars.__getitem__)
+        invars = list(map(invars.__getitem__, perm))
+        vecs = list(map(vecs.__getitem__, perm))
+        changed = False
+        for i in range(1, len(vecs)):
+            d1, v1 = invars[i-1], vecs[i-1]
+            d2, v2 = invars[i], vecs[i]
+            if d2 % d1 != 0:
+                d1, v1, d2, v2 = _normalize_pair(d1, v1, d2, v2)
+                invars[i-1], vecs[i-1] = d1, v1
+                invars[i], vecs[i] = d2, v2
+                changed = True
+        if not changed:
+            return invars + [0] * len(zeros), vecs + zeros
+
+def _smith_with_coefficients(N, vectors):
+    """Find the SNF of a matrix, along with the matrix of row operations used.
+
+    Given a matrix A of row vectors, apply row operations to [A | id],
+    along with column operations to A only, to get a matrix [D | S],
+    where D=S*A*T is in SNF, T (not provided) is the matrix of column operations used.
+    Return the diagonal entries of D, along with the row vectors of S.
+    """
+    invariants, coefficient_vectors = _smith_with_coefficients_unnormalized(N, vectors)
+    return _normalize_smith_with_coefficients(invariants, coefficient_vectors)
+
+def _cokernel_with_generators_unfiltered(N, vectors):
+    """
+    Given vectors in Z^N, produce Z^N/(Z-span of vectors) as
+    a list of cyclic summands (invariant factors), along with a vector in Z^N
+    representing each cyclic summand. Include trivial summands Z/1.
+    """
+    vectors = _hnf(vectors)
+    R = len(vectors)
+    columns = transpose(N, vectors)
+    invariants, column_coefficient_vectors = _smith_with_coefficients(R, columns)
+    return invariants, _inverse(transpose(N, column_coefficient_vectors))
+
+def cokernel_with_generators(N, vectors):
+    """
+    Given vectors in Z^N, produce Z^N/(Z-span of vectors) as
+    a list of cyclic summands (invariant factors), along with a vector in Z^N
+    representing each cyclic summand. Do NOT include trivial summands Z/1.
+    """
+    invariants, generators = _cokernel_with_generators_unfiltered(N, vectors)
+    ones = sum(1 for d in invariants if d == 1)
+    assert invariants[:ones] == [1] * ones
+    return invariants[ones:], generators[ones:]
+
+def homology_with_generators(N, incoming, K, outgoing):
+    """Compute the homology at Z^R ---> Z^N ---> Z^K.
+
+    `incoming` is a list of length R specifying the image in Z^N of each basis element of Z^R.
+    `outgoing` is likewise a list of length N of vectors of length K.
+    """
+    kernel = relations_among(outgoing)
+    relative = list(map(kernel.coefficients_of, incoming))
+    invariants, rel_generators = cokernel_with_generators(kernel.rank, relative)
+    generators = list(map(kernel.linear_combination, rel_generators))
+    return invariants, generators
---
+++ test/test_homology_with_generators.py
@@ -0,0 +1,189 @@
+import random
+import itertools
+
+from mutable_lattice import Vector, Lattice, transpose, relations_among
+from math import gcd
+from fast_semigroup_homology.cup_products.homology_with_generators import (
+    _smith_with_coefficients_unnormalized,
+    _normalize_pair,
+    _normalize_smith_with_coefficients,
+    _smith_with_coefficients,
+    _cokernel_with_generators_unfiltered,
+    cokernel_with_generators,
+    _inverse,
+    homology_with_generators,
+)
+
+def test__inverse():
+    assert _inverse([]) == []
+    assert _inverse([Vector([1])]) == [Vector([1])]
+    assert _inverse([Vector([-1])]) == [Vector([-1])]
+    assert _inverse([Vector([1,0]),Vector([0,1])]) == [Vector([1,0]),Vector([0,1])]
+    assert _inverse([Vector([1,0]),Vector([0,-1])]) == [Vector([1,0]),Vector([0,-1])]
+    assert _inverse([Vector([1,5]),Vector([0,1])]) == [Vector([1,-5]),Vector([0,1])]
+    assert _inverse([Vector([1,5]),Vector([0,1])]) == [Vector([1,-5]),Vector([0,1])]
+    assert _inverse([Vector([9,4]),Vector([11,5])]) == [Vector([5,-4]),Vector([-11,9])]
+    assert _inverse([Vector([11,5]),Vector([9,4])]) == [Vector([-4,5]),Vector([9,-11])]
+
+def test_smith_with_coefficients_unnormalized():
+    assert _smith_with_coefficients_unnormalized(0, []) == ([], [])
+    assert _smith_with_coefficients_unnormalized(1, []) == ([], [])
+    assert _smith_with_coefficients_unnormalized(2, []) == ([], [])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([1])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([-1])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([5])]) == ([5], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([-5])]) == ([5], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([0])]) == ([0], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(1, [Vector([5]), Vector([5])]) == ([5, 0], [Vector([0,1]),Vector([1,-1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([0,0])]) == ([0], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([1,0])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([1,0]), Vector([2, 2])]) == ([1, 2], [Vector([1,0]),Vector([0,1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([1,0]), Vector([2, 2]), Vector([0,2])]) == ([1, 2, 0], [Vector([1,0,0]),Vector([0,1,0]),Vector([2,-1,1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([2,0]), Vector([3, 3])]) == ([2, 3], [Vector([1,0]),Vector([0,1])])
+    assert _smith_with_coefficients_unnormalized(2, [Vector([-5,0]), Vector([5,4])]) == ([1, 20], [Vector([-1,2]),Vector([-3,5])])
+
+def test_normalize_pair():
+    assert _normalize_pair(2, Vector([10,0]), 3, Vector([0,10])) == (1, Vector([-10,10]), 6, Vector([-30,20]))
+
+def test_normalize_pair_random():
+    vectors = [Vector([x, x+2]) for x in range(-3, 4)]
+    pairs = [(d1, d2) for d1 in range(1, 7) for d2 in range(1, 7) if d2 % d1 != 0]
+    for v1 in vectors:
+        for v2 in vectors:
+            for d1, d2 in pairs:
+                d3, v3, d4, v4 = _normalize_pair(d1, v1, d2, v2)
+                assert Lattice(2, [v1, v2]) == Lattice(2, [v3, v4])
+                assert d3 == gcd(d1, d2)
+                assert d4 == d1*d2 // d3
+
+def test__smith_with_coefficients():
+    assert _smith_with_coefficients(0, []) == ([], [])
+    assert _smith_with_coefficients(1, []) == ([], [])
+    assert _smith_with_coefficients(2, []) == ([], [])
+    assert _smith_with_coefficients(1, [Vector([1])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients(1, [Vector([-1])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients(1, [Vector([5])]) == ([5], [Vector([1])])
+    assert _smith_with_coefficients(1, [Vector([-5])]) == ([5], [Vector([1])])
+    assert _smith_with_coefficients(1, [Vector([0])]) == ([0], [Vector([1])])
+    assert _smith_with_coefficients(1, [Vector([5]), Vector([5])]) == ([5, 0], [Vector([0,1]),Vector([1,-1])])
+    assert _smith_with_coefficients(2, [Vector([0,0])]) == ([0], [Vector([1])])
+    assert _smith_with_coefficients(2, [Vector([1,0])]) == ([1], [Vector([1])])
+    assert _smith_with_coefficients(2, [Vector([1,0]), Vector([2,2])]) == ([1, 2], [Vector([1,0]),Vector([0,1])])
+    assert _smith_with_coefficients(2, [Vector([1,0]), Vector([2,2]), Vector([0,2])]) == ([1, 2, 0], [Vector([1,0,0]),Vector([0,1,0]),Vector([2,-1,1])])
+    assert _smith_with_coefficients(2, [Vector([2,0]), Vector([3,3])]) == ([1, 6], [Vector([-1,1]),Vector([-3,2])])
+    assert _smith_with_coefficients(2, [Vector([-5,0]), Vector([5,4])]) == ([1, 20], [Vector([-1,2]),Vector([-3,5])])
+
+def test_smith_with_coefficients_random():
+    for N, R, _ in itertools.product(range(5), repeat=3):
+        data = [Vector([random.randint(-10, 10) for _ in range(N)]) for _ in range(R)]
+        invariants, coefficient_vectors = _smith_with_coefficients(N, data)
+        nonzero_invariants = [d for d in invariants if d]
+        assert invariants == Lattice(R, transpose(N, data)).invariants()
+        assert nonzero_invariants == Lattice(N, data).nonzero_invariants()
+        # transformation was invertible
+        assert Lattice(R, coefficient_vectors).invariants() == [1] * R
+        combos = []
+        for d, w in zip(invariants, coefficient_vectors):
+            v = Vector.zero(N)
+            for c, vi in zip(w, data):
+                v += c * vi
+            if d == 0:
+                assert not any(v)
+            else:
+                assert Lattice(N, [v], maxrank=1).nonzero_invariants() == [d]
+                combos.append(v)
+        assert Lattice(N, combos).nonzero_invariants() == nonzero_invariants
+
+def test_cokernel_with_generators():
+    assert cokernel_with_generators(0, []) == ([], [])
+    assert cokernel_with_generators(1, []) == ([0], [Vector([1])])
+    assert cokernel_with_generators(2, []) == ([0, 0], [Vector([1,0]), Vector([0,1])])
+    assert cokernel_with_generators(1, [Vector([1])]) == ([], [])
+    assert cokernel_with_generators(1, [Vector([-1])]) == ([], [])
+    assert cokernel_with_generators(1, [Vector([5])]) == ([5], [Vector([1])])
+    assert cokernel_with_generators(1, [Vector([-5])]) == ([5], [Vector([1])])
+    assert cokernel_with_generators(1, [Vector([0])]) == ([0], [Vector([1])])
+    assert cokernel_with_generators(1, [Vector([5]), Vector([5])]) == ([5], [Vector([1])])
+    assert cokernel_with_generators(2, [Vector([0,0])]) == ([0, 0], [Vector([1,0]),Vector([0,1])])
+    assert cokernel_with_generators(2, [Vector([1,0])]) == ([0], [Vector([0,1])])
+    assert cokernel_with_generators(2, [Vector([2,-1])]) == ([0], [Vector([1,0])])
+    assert cokernel_with_generators(2, [Vector([1,0]),Vector([2,2])]) == ([2], [Vector([0,1])])
+    assert cokernel_with_generators(2, [Vector([1,0]),Vector([2,2]),Vector([0,2])]) == ([2], [Vector([0,1])])
+    assert cokernel_with_generators(2, [Vector([2,0]),Vector([3,3])]) == ([6], [Vector([0,1])])
+    assert cokernel_with_generators(2, [Vector([-5,0]),Vector([5,4])]) == ([20], [Vector([-1,-1])])
+
+def test_cokernel_with_generators_random():
+    for N, R, _ in itertools.product(range(5), repeat=3):
+        data = [Vector([random.randint(-10, 10) for _ in range(N)]) for _ in range(R)]
+        invariants, generators = _cokernel_with_generators_unfiltered(N, data)
+        L = Lattice(N, data)
+        L0 = L.copy()
+        assert invariants == L.nonzero_invariants() + [0] * (N - L.rank)
+        for d, v in zip(invariants, generators):
+            for k in range(1, d):
+                assert k*v not in L
+                assert k*v not in L0
+            assert d*v in L
+            assert d*v in L0
+            L.add_vector(v)
+        assert L.is_full()
+
+def test_homology_with_generators():
+    assert homology_with_generators(
+        2, [Vector([0,0]), Vector([0,0]), Vector([0,0])],
+        4, [Vector([7,7,7,7]),Vector([0,0,5,5])]
+    ) == ([], [])
+    assert homology_with_generators(
+        2, [Vector([0,0])],
+        4, [Vector([7,7,7,7]),Vector([7,7,7,7])]
+    ) == ([0], [Vector([1,-1])])
+    assert homology_with_generators(
+        2, [Vector([0,0])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3])]
+    ) == ([0], [Vector([3,-2])])
+    assert homology_with_generators(
+        3, [Vector([0,0,0])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([0, 0], [Vector([3,-2, 0]), Vector([0, 0, 1])])
+    assert homology_with_generators(
+        3, [Vector([3,-2,0])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([0], [Vector([0, 0, 1])])
+    assert homology_with_generators(
+        3, [Vector([0,0,1])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([0], [Vector([3,-2,0])])
+    assert homology_with_generators(
+        3, [Vector([0,0,1]), Vector([3,-2,1])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([], [])
+    assert homology_with_generators(
+        3, [Vector([3,-2,-100])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([0], [Vector([0,0,1])])
+    assert homology_with_generators(
+        3, [Vector([-30,20,1])],
+        4, [Vector([2,2,2,2]),Vector([3,3,3,3]),Vector([0,0,0,0])]
+    ) == ([0], [Vector([3,-2,0])])
+
+def test_homology_with_generators_random():
+    for K, N, R, _ in itertools.product(range(5), repeat=4):
+        outgoing = [Vector([random.randint(-10,10) for _ in range(K)])
+                    for _ in range(N)]
+        kernel = relations_among(outgoing)
+        incoming = [kernel.linear_combination(
+                        Vector([random.randint(-10,10)
+                                for _ in range(kernel.rank)]))
+                    for _ in range(R)]
+        invariants, generators = homology_with_generators(N, incoming, K, outgoing)
+        L = Lattice(N, incoming)
+        L0 = L.copy()
+        assert invariants == [d for d in L.nonzero_invariants() if d != 1] + [0] * (kernel.rank - L.rank)
+        for d, v in zip(invariants, generators):
+            for k in range(1, d):
+                assert k*v not in L
+                assert k*v not in L0
+            assert d*v in L
+            assert d*v in L0
+            L.add_vector(v)
+        assert L == kernel
